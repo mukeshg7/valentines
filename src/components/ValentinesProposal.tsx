b630203c127729f565873cee754ee26dcdb180fe
Mukesh Gupta <mukeshgupta202000@gmail.com>
2026-02-14 23:32:22 +0530

    fix: add ./ path to the images for hamster images

--- src/components/ValentinesProposal.tsx
+++ src/components/ValentinesProposal.tsx
@@ -135,7 +135,7 @@ export default function ValentinesProposal() {
               Will you be my Valentine?
             </h2>
             <Image
-              src="/sad_hamster.png"
+              src="./sad_hamster.png"
               alt="Sad Hamster"
               width={200}
               height={200}
@@ -178,7 +178,7 @@ export default function ValentinesProposal() {
             Thank you for accepting, I love you! 💕
             <p className="text-sm mt-4">For more information, write me!!! 💌</p>
             <Image
-              src="/hamster_jumping.gif"
+              src="./hamster_jumping.gif"
               alt="Hamster Feliz"
               width={200}
               height={200}
